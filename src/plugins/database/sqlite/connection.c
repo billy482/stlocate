953f5f6e5313892b913b2941387a9ee3c4a1fd05
Clercin Guillaume <gclercin@intellique.com>
2013-07-20 14:16:45 +0200

    Change db model

--- src/plugins/database/sqlite/connection.c
+++ src/plugins/database/sqlite/connection.c
@@ -22,7 +22,7 @@
 *                                                                         *
 *  ---------------------------------------------------------------------  *
 *  Copyright (C) 2013, Clercin guillaume <gclercin@intellique.com>        *
-*  Last modified: Fri, 19 Jul 2013 23:04:03 +0200                         *
+*  Last modified: Sat, 20 Jul 2013 14:01:44 +0200                         *
 \*************************************************************************/
 
 // free, malloc
@@ -50,7 +50,7 @@ static int sl_database_sqlite_connection_finish_transaction(struct sl_database_c
 static int sl_database_sqlite_connection_start_transaction(struct sl_database_connection * connect);
 
 static int sl_database_sqlite_connection_create_database(struct sl_database_connection * connect, int version);
-static int sl_database_sqlite_connection_create_table(sqlite3 * db, const char * table, const char * query);
+static int sl_database_sqlite_connection_exec(sqlite3 * db, const char * query);
 static int sl_database_sqlite_connection_get_database_version(struct sl_database_connection * connect);
 
 static int sl_database_sqlite_connection_delete_old_session(struct sl_database_connection * connect, int nb_keep_sesion);
@@ -88,6 +88,8 @@ struct sl_database_connection * sl_database_sqlite_connection_add(struct sl_data
 		sl_log_write(sl_log_level_err, sl_log_type_plugin_database, "Sqlite: failed to open database at '%s'", path);
 		return NULL;
 	}
+
+	sl_database_sqlite_connection_exec(handler, "PRAGMA foreign_keys = ON");
 
 	struct sl_database_sqlite_connection_private * self = malloc(sizeof(struct sl_database_sqlite_connection_private));
 	self->db_handler = handler;
@@ -205,31 +207,31 @@ static int sl_database_sqlite_connection_create_database(struct sl_database_conn
 		return 1;
 	}
 
-	int failed = sl_database_sqlite_connection_create_table(self->db_handler, "host", "CREATE TABLE host (id INTEGER PRIMARY KEY, name TEXT UNIQUE)");
+	int failed = sl_database_sqlite_connection_exec(self->db_handler, "CREATE TABLE host (id INTEGER PRIMARY KEY, name TEXT UNIQUE)");
 	if (failed)
 		return failed;
 
-	failed = sl_database_sqlite_connection_create_table(self->db_handler, "session", "CREATE TABLE session (id INTEGER PRIMARY KEY, start_time INTEGER NOT NULL, end_time INTEGER NULL, host INTEGER NOT NULL REFERENCES host(id) ON UPDATE CASCADE ON DELETE CASCADE, CHECK (start_time <= end_time))");
+	failed = sl_database_sqlite_connection_exec(self->db_handler, "CREATE TABLE session (id INTEGER PRIMARY KEY, start_time INTEGER NOT NULL, end_time INTEGER NULL, host INTEGER NOT NULL REFERENCES host(id) ON UPDATE CASCADE ON DELETE CASCADE, CHECK (start_time <= end_time))");
 	if (failed)
 		return failed;
 
-	failed = sl_database_sqlite_connection_create_table(self->db_handler, "filesystem", "CREATE TABLE filesystem (id INTEGER PRIMARY KEY, uuid TEXT NOT NULL UNIQUE, label TEXT, type TEXT NOT NULL)");
+	failed = sl_database_sqlite_connection_exec(self->db_handler, "CREATE TABLE filesystem (id INTEGER PRIMARY KEY, uuid TEXT NOT NULL UNIQUE, label TEXT, type TEXT NOT NULL)");
 	if (failed)
 		return failed;
 
-	failed = sl_database_sqlite_connection_create_table(self->db_handler, "session2filesystem", "CREATE TABLE session2filesystem (id INTEGER PRIMARY KEY, session INTEGER NOT NULL REFERENCES session(id) ON UPDATE CASCADE ON DELETE CASCADE, filesystem INTEGER NOT NULL REFERENCES filesystem(id) ON UPDATE CASCADE ON DELETE CASCADE, mount_point TEXT NOT NULL, dev_no INTEGER NOT NULL CHECK (dev_no >= 0), disk_free INTEGER NOT NULL CHECK (disk_free >= 0), disk_total INTEGER NOT NULL CHECK (disk_total >= 0), block_size INTEGER NOT NULL CHECK (block_size >= 0), CHECK (disk_free <= disk_total))");
+	failed = sl_database_sqlite_connection_exec(self->db_handler, "CREATE TABLE session2filesystem (id INTEGER PRIMARY KEY, session INTEGER NOT NULL REFERENCES session(id) ON UPDATE CASCADE ON DELETE CASCADE, filesystem INTEGER NOT NULL REFERENCES filesystem(id) ON UPDATE CASCADE ON DELETE CASCADE, mount_point TEXT NOT NULL, dev_no INTEGER NOT NULL CHECK (dev_no >= 0), disk_free INTEGER NOT NULL CHECK (disk_free >= 0), disk_total INTEGER NOT NULL CHECK (disk_total >= 0), block_size INTEGER NOT NULL CHECK (block_size >= 0), CHECK (disk_free <= disk_total))");
 	if (failed)
 		return failed;
 
-	failed = sl_database_sqlite_connection_create_table(self->db_handler, "file", "CREATE TABLE file (id INTEGER PRIMARY KEY, path TEXT NOT NULL, inode INTEGER NOT NULL CHECK (inode >= 0), UNIQUE (path, inode))");
+	failed = sl_database_sqlite_connection_exec(self->db_handler, "CREATE TABLE file (s2fs INTEGER NOT NULL REFERENCES session2filesystem(id) ON UPDATE CASCADE ON DELETE CASCADE, inode INTEGER NOT NULL CHECK (inode >= 0), path TEXT NOT NULL, mode INTEGER NOT NULL CHECK (mode >= 0), uid INTEGER NOT NULL CHECK (uid >= 0), gid INTEGER NOT NULL CHECK (gid >= 0), size INTEGER NOT NULL CHECK (size >= 0), access_time INTEGER NOT NULL, modif_time INTEGER NOT NULL)");
 	if (failed)
 		return failed;
 
-	failed = sl_database_sqlite_connection_create_table(self->db_handler, "file2session", "CREATE TABLE file2session (s2fs INTEGER NOT NULL REFERENCES session2filesystem(id) ON UPDATE CASCADE ON DELETE CASCADE, file INTEGER NOT NULL REFERENCES file(id) ON UPDATE CASCADE ON DELETE CASCADE, mode INTEGER NOT NULL CHECK (mode >= 0), uid INTEGER NOT NULL CHECK (uid >= 0), gid INTEGER NOT NULL CHECK (gid >= 0), size INTEGER NOT NULL CHECK (size >= 0), access_time INTEGER NOT NULL, modif_time INTEGER NOT NULL)");
+	failed = sl_database_sqlite_connection_exec(self->db_handler, "CREATE INDEX inode ON file(s2fs, inode)");
 	if (failed)
 		return failed;
 
-	failed = sl_database_sqlite_connection_create_table(self->db_handler, "config", "CREATE TABLE config (key TEXT NOT NULL UNIQUE, VALUE TEXT)");
+	failed = sl_database_sqlite_connection_exec(self->db_handler, "CREATE TABLE config (key TEXT NOT NULL UNIQUE, VALUE TEXT)");
 	if (failed)
 		return failed;
 
@@ -247,12 +249,12 @@ static int sl_database_sqlite_connection_create_database(struct sl_database_conn
 	return (failed != SQLITE_DONE);
 }
 
-static int sl_database_sqlite_connection_create_table(sqlite3 * db, const char * table, const char * query) {
+static int sl_database_sqlite_connection_exec(sqlite3 * db, const char * query) {
 	char * error = NULL;
 	int failed = sqlite3_exec(db, query, NULL, NULL, &error);
 
 	if (failed != SQLITE_OK) {
-		sl_log_write(sl_log_level_err, sl_log_type_plugin_database, "Sqlite: failed to create table '%s' with query '%s' because %s", table, query, error);
+		sl_log_write(sl_log_level_err, sl_log_type_plugin_database, "Sqlite: failed to execute query %s because %s", query, error);
 		sqlite3_free(error);
 	}
 
@@ -310,87 +312,26 @@ static int sl_database_sqlite_connection_delete_old_session(struct sl_database_c
 		return -1;
 	}
 
-	sqlite3_stmt * stmt_ds;
-	static const char * query_ds = "DELETE FROM session WHERE id IN (SELECT session FROM remove_session)";
-	failed = sqlite3_prepare_v2(self->db_handler, query_ds, -1, &stmt_ds, NULL);
-	if (failed) {
-		sl_log_write(sl_log_level_err, sl_log_type_plugin_database, "Sqlite: error while preparing query 'delete from session'");
-		return -1;
-	}
-
-	failed = sqlite3_step(stmt_ds);
-	sqlite3_finalize(stmt_ds);
-
-	if (failed != SQLITE_DONE) {
-		sl_log_write(sl_log_level_err, sl_log_type_plugin_database, "Sqlite: error while delete from session");
-		return -1;
-	}
-
-	sqlite3_stmt * stmt_dtrs;
-	static const char * query_dtrs = "DROP TABLE remove_session";
-	failed = sqlite3_prepare_v2(self->db_handler, query_dtrs, -1, &stmt_dtrs, NULL);
-	if (failed) {
-		sl_log_write(sl_log_level_err, sl_log_type_plugin_database, "Sqlite: error while preparing query 'drop table remove_session'");
-		return -1;
-	}
-
-	failed = sqlite3_step(stmt_dtrs);
-	sqlite3_finalize(stmt_dtrs);
-
-	if (failed != SQLITE_DONE) {
-		sl_log_write(sl_log_level_err, sl_log_type_plugin_database, "Sqlite: error while dropping table remove_session");
-		return -1;
-	}
-
-	sqlite3_stmt * stmt_ds2fs;
-	static const char * query_ds2fs = "DELETE FROM session2filesystem WHERE session NOT IN (SELECT id FROM session)";
-	failed = sqlite3_prepare_v2(self->db_handler, query_ds2fs, -1, &stmt_ds2fs, NULL);
-	if (failed) {
-		sl_log_write(sl_log_level_err, sl_log_type_plugin_database, "Sqlite: error while preparing query 'delete from session2filesystem'");
-		return -1;
-	}
-
-	failed = sqlite3_step(stmt_ds2fs);
-	sqlite3_finalize(stmt_ds2fs);
-
-	if (failed != SQLITE_DONE) {
-		sl_log_write(sl_log_level_err, sl_log_type_plugin_database, "Sqlite: error while deleting from session2filesystem");
-		return -1;
-	}
-
-	sqlite3_stmt * stmt_df2s;
-	static const char * query_df2s = "DELETE FROM file2session WHERE s2fs NOT IN (SELECT id FROM session2filesystem)";
-	failed = sqlite3_prepare_v2(self->db_handler, query_df2s, -1, &stmt_df2s, NULL);
-	if (failed) {
-		sl_log_write(sl_log_level_err, sl_log_type_plugin_database, "Sqlite: error while preparing query 'delete from file2session'");
-		return -1;
-	}
-
-	failed = sqlite3_step(stmt_df2s);
-	sqlite3_finalize(stmt_df2s);
-
-	if (failed != SQLITE_DONE) {
-		sl_log_write(sl_log_level_err, sl_log_type_plugin_database, "Sqlite: error while deleting from file2session");
-		return -1;
-	}
-
-	sqlite3_stmt * stmt_df;
-	static const char * query_df = "DELETE FROM file WHERE id NOT IN (SELECT file FROM file2session)";
-	failed = sqlite3_prepare_v2(self->db_handler, query_df, -1, &stmt_df, NULL);
-	if (failed) {
-		sl_log_write(sl_log_level_err, sl_log_type_plugin_database, "Sqlite: error while preparing query 'delete from file'");
-		return -1;
-	}
-
-	failed = sqlite3_step(stmt_df);
-	sqlite3_finalize(stmt_df);
-
-	if (failed != SQLITE_DONE) {
-		sl_log_write(sl_log_level_err, sl_log_type_plugin_database, "Sqlite: error while deleting from file");
-		return -1;
-	}
-
 	char * error;
+	failed = sqlite3_exec(self->db_handler, "DELETE FROM session WHERE id IN (SELECT session FROM remove_session)", NULL, NULL, &error);
+	if (failed != SQLITE_OK) {
+		sl_log_write(sl_log_level_err, sl_log_type_plugin_database, "Sqlite: error while preparing query 'delete from session' because %s", error);
+		sqlite3_free(error);
+		return -1;
+	}
+
+	int nb_changed = sqlite3_changes(self->db_handler);
+
+	failed = sqlite3_exec(self->db_handler, "DROP TABLE remove_session", NULL, NULL, &error);
+	if (failed != SQLITE_OK) {
+		sl_log_write(sl_log_level_err, sl_log_type_plugin_database, "Sqlite: error while dropping table remove_session because %s", error);
+		sqlite3_free(error);
+		return -1;
+	}
+
+	if (nb_changed < 1)
+		return 0;
+
 	failed = sqlite3_exec(self->db_handler, "VACUUM", NULL, NULL, &error);
 
 	if (failed != SQLITE_OK) {
@@ -494,74 +435,29 @@ static int sl_database_sqlite_connection_sync_file(struct sl_database_connection
 	if (self->db_handler == NULL)
 		return 1;
 
-	sqlite3_stmt * stmt_select;
-	static const char * query = "SELECT id FROM file WHERE path = ?1 AND inode = ?2 LIMIT 1";
-	int failed = sqlite3_prepare_v2(self->db_handler, query, -1, &stmt_select, NULL);
-	if (failed) {
-		sl_log_write(sl_log_level_err, sl_log_type_plugin_database, "Sqlite: error while preparing query 'get file by path and inode'");
-		return -1;
-	}
-
-	sqlite3_bind_text(stmt_select, 1, filename, -1, SQLITE_STATIC);
-	sqlite3_bind_int64(stmt_select, 2, st->st_ino);
-
-	long long file_id = -1;
-	failed = sqlite3_step(stmt_select);
-	if (failed == SQLITE_ROW) {
-		file_id = sqlite3_column_int64(stmt_select, 0);
-		sqlite3_finalize(stmt_select);
-	} else if (failed == SQLITE_DONE) {
-		sqlite3_finalize(stmt_select);
-
-		static const char * insert = "INSERT INTO file(path, inode) VALUES (?1, ?2)";
-		sqlite3_stmt * stmt_insert;
-		failed = sqlite3_prepare_v2(self->db_handler, insert, -1, &stmt_insert, NULL);
-		if (failed) {
-			sl_log_write(sl_log_level_err, sl_log_type_plugin_database, "Sqlite: error while preparing query 'insert into file'");
-			sqlite3_finalize(stmt_select);
-			return -2;
-		}
-
-		sqlite3_bind_text(stmt_insert, 1, filename, -1, SQLITE_STATIC);
-		sqlite3_bind_int64(stmt_insert, 2, st->st_ino);
-		failed = sqlite3_step(stmt_insert);
-
-		if (failed == SQLITE_DONE)
-			file_id = sqlite3_last_insert_rowid(self->db_handler);
-		else
-			sl_log_write(sl_log_level_err, sl_log_type_plugin_database, "Sqlite: failed to insert new file (path: %s, inode: %ld)", filename, st->st_ino);
-
-		sqlite3_finalize(stmt_insert);
-	} else {
-		sl_log_write(sl_log_level_err, sl_log_type_plugin_database, "Sqlite: failed to get a file id");
-		return -3;
-	}
-
-	if (file_id < 0)
-		return -4;
-
-	static const char * insert = "INSERT INTO file2session(s2fs, file, mode, uid, gid, size, access_time, modif_time) VALUES (?1, ?2, ?3, ?4, ?5, ?6, datetime(?7, 'unixepoch'), datetime(?8, 'unixepoch'))";
+	static const char * insert = "INSERT INTO file(s2fs, inode, path, mode, uid, gid, size, access_time, modif_time) VALUES (?1, ?2, ?3, ?4, ?5, ?6, ?7, datetime(?8, 'unixepoch'), datetime(?9, 'unixepoch'))";
 	sqlite3_stmt * stmt_insert;
-	failed = sqlite3_prepare_v2(self->db_handler, insert, -1, &stmt_insert, NULL);
+	int failed = sqlite3_prepare_v2(self->db_handler, insert, -1, &stmt_insert, NULL);
 	if (failed) {
 		sl_log_write(sl_log_level_err, sl_log_type_plugin_database, "Sqlite: error while preparing query 'insert into file2session'");
-		sqlite3_finalize(stmt_select);
+		sqlite3_finalize(stmt_insert);
 		return -4;
 	}
 
 	sqlite3_bind_int64(stmt_insert, 1, s2fs);
-	sqlite3_bind_int64(stmt_insert, 2, file_id);
-	sqlite3_bind_int(stmt_insert, 3, st->st_mode);
-	sqlite3_bind_int(stmt_insert, 4, st->st_uid);
-	sqlite3_bind_int(stmt_insert, 5, st->st_gid);
-	sqlite3_bind_int64(stmt_insert, 6, st->st_size);
-	sqlite3_bind_int64(stmt_insert, 7, st->st_atime);
-	sqlite3_bind_int64(stmt_insert, 8, st->st_mtime);
+	sqlite3_bind_int64(stmt_insert, 2, st->st_ino);
+	sqlite3_bind_text(stmt_insert, 3, filename, -1, SQLITE_STATIC);
+	sqlite3_bind_int(stmt_insert, 4, st->st_mode);
+	sqlite3_bind_int(stmt_insert, 5, st->st_uid);
+	sqlite3_bind_int(stmt_insert, 6, st->st_gid);
+	sqlite3_bind_int64(stmt_insert, 7, st->st_size);
+	sqlite3_bind_int64(stmt_insert, 8, st->st_atime);
+	sqlite3_bind_int64(stmt_insert, 9, st->st_mtime);
 
 	failed = sqlite3_step(stmt_insert);
 
 	if (failed != SQLITE_DONE)
-		sl_log_write(sl_log_level_err, sl_log_type_plugin_database, "Sqlite: failed to insert new file2session (file id: %lld, session id: %d)", file_id, s2fs);
+		sl_log_write(sl_log_level_err, sl_log_type_plugin_database, "Sqlite: failed to insert new file2session (file id: 0, session id: %d)", s2fs);
 
 	sqlite3_finalize(stmt_insert);
 
